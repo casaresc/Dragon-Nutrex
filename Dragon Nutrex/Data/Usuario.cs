010627dd7e47d47eae2a6fe9b786ae876377473a
Joshua Arcia <casares261@gmail.com>
2026-03-23 23:43:51 -0600

    Created user model

---
+++ Dragon Nutrex/Data/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dragon_Nutrex.Data
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+        public string Objetivo { get; set; }
+        public string NivelActividad { get; set; }
+    }
+}
